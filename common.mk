8412ae74e8f890983ca0e80c41031c82c2798bc4
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    hi3650: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- common.mk
+++ common.mk
@@ -305,7 +305,10 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     $(LOCAL_PATH)/resources
-    
+
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
