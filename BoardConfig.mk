7557b230dd9be36ee36d73c1165f623c1a4f9f7c
arco <arco68@gmail.com>
2023-06-17 22:39:11 +0000

    Silence clang error during kernel compile
    * clang-14: error: unable to execute command: Executable "ld" doesn't exist!
    
    Change-Id: I1ebf0fb665704424a8ad3a375c546c6cca44dc0b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,6 +91,8 @@ TARGET_KERNEL_CONFIG := hi3650_defconfig
 TARGET_KERNEL_CLANG_VERSION := r416183b
 TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/kernel/$(HOST_PREBUILT_TAG)/clang-$(TARGET_KERNEL_CLANG_VERSION)
 TARGET_KERNEL_LLVM_BINUTILS := false
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
