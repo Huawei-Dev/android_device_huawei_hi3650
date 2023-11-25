fb57a569251585ea2f18cd0f11a89c9bacf37539
Huawei-Dev <surdu_petru@yahoo.com>
2023-11-25 22:52:24 +0100

    add vr permissions

--- device.mk
+++ device.mk
@@ -325,6 +325,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
     android.hardware.vr@1.0-service
+    
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vr.high_performance.xml
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
