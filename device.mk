d3bb9e5ad8e71e89dc78f5531b9b8ea558c6f20a
Huawei-Dev <surdu_petru@yahoo.com>
2023-11-28 14:42:26 +0100

    update audio to 4.0

--- device.mk
+++ device.mk
@@ -42,7 +42,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.broadcastradio@1.0-impl
 
 PRODUCT_COPY_FILES += \
