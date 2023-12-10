8ce8ead54c962e8c9229cbe9bd3b295011a1d4a8
Huawei-Dev <surdu_petru@yahoo.com>
2023-12-10 12:42:28 +0100

    build android.hardware.light-service.hisi

--- device.mk
+++ device.mk
@@ -121,9 +121,7 @@ PRODUCT_PACKAGES += \
     
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.hi3650
+    android.hardware.light-service.hisi
     
 # Lineage hardware
 PRODUCT_PACKAGES += \
