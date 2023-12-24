81cf81043ba0fe7e5f721d951aeb36759eced0a7
Huawei-Dev <surdu_petru@yahoo.com>
2023-12-24 14:18:07 +0100

    revert commit : 4c62813

--- device.mk
+++ device.mk
@@ -92,7 +92,8 @@ PRODUCT_PACKAGES += \
     
 # Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-service.software
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
 
 # GPS
 PRODUCT_COPY_FILES += \
