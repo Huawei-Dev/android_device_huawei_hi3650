98262beb50e39d9619420c82ae277824d07cc4d6
Huawei-Dev <surdu_petru@yahoo.com>
2024-01-03 16:52:22 +0100

    switch AAPT_PREF_CONFIG to xhdpi

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@ LOCAL_PATH := device/huawei/beethoven
 
 # AAPT conf
 PRODUCT_AAPT_CONFIG := normal 
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_CHARACTERISTICS := tablet
 
 # Additional native libraries
