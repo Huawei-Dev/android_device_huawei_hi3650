570cfd3d00391ba5ea49b1c8ea599d4dc3f86c20
Huawei-Dev <surdu_petru@yahoo.com>
2024-01-22 20:30:14 +0100

    hi3650: stop copy prebuilt vndk28 libui

--- common.mk
+++ common.mk
@@ -323,8 +323,7 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor
     
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-lite-vendorcompat \
-    libui-v28
+    libprotobuf-cpp-lite-vendorcompat
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
