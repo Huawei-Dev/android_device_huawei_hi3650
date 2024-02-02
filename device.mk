a5ec8f3020d014e9ffb15d336ded2ce2e35ed504
R0rt1z2 <me@r0rt1z2.com>
2024-02-02 10:54:51 +0100

    Build hisi `libril` wrapper

--- device.mk
+++ device.mk
@@ -275,6 +275,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.radio@1.0.vendor \
+    libril \
     librilutils \
     mploader
     
