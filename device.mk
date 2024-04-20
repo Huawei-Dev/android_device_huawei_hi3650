67cd09f3a9e22f93c0a5bbe05e74bd352cd2a5a7
Huawei_Dev <surdu_petru@yahoo.com>
2024-04-20 09:40:03 +0200

    beethoven: move privapp_whitelist_com.android.supl.xml to /system

--- device.mk
+++ device.mk
@@ -118,7 +118,7 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/privapp_whitelist_com.android.supl.xml:$(TARGET_COPY_OUT_SYSTEM)/system_ext/etc/permissions/privapp_whitelist_com.android.supl.xml
+    $(LOCAL_PATH)/prebuilts/privapp_whitelist_com.android.supl.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp_whitelist_com.android.supl.xml
     
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.0.vendor \
