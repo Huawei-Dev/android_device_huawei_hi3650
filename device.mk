9cb16285f4bbe352c17bc34ac72ac09089ff9cce
Huawei_Dev <surdu_petru@yahoo.com>
2024-04-21 21:34:52 +0200

    beethoven: move wpa_supplicant_overlay.conf from vendor blobs to device/tree

--- device.mk
+++ device.mk
@@ -341,6 +341,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/cfg_btv_hisi.ini:$(TARGET_COPY_OUT_VENDOR)/etc/cfg_btv_hisi.ini \
     $(LOCAL_PATH)/prebuilts/cfg_btv_wifi_only_hisi.ini:$(TARGET_COPY_OUT_VENDOR)/etc/cfg_btv_wifi_only_hisi.ini
     
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+    
 include hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
