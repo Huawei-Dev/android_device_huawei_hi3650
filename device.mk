fe75ab728215f684124bc36876ae62950d4219f4
Huawei_Dev <surdu_petru@yahoo.com>
2024-04-28 10:38:56 +0200

    beethoven: switch gnss to EMUI9

--- device.mk
+++ device.mk
@@ -121,9 +121,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/privapp_whitelist_com.android.supl.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp_whitelist_com.android.supl.xml
     
 PRODUCT_PACKAGES += \
-    android.hardware.gnss.measurement_corrections@1.0.vendor \
-    android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss@2.0.vendor
+    android.hardware.gnss@1.1.vendor
     
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor
