e98a7d5a58c39408609ae3a92774d9069bebbdac
Huawei_Dev <surdu_petru@yahoo.com>
2024-05-12 09:10:13 +0200

    hi3650: build libbt-vendor

--- common.mk
+++ common.mk
@@ -75,6 +75,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0.vendor
     
+PRODUCT_PACKAGES += \
+    libbt-vendor
+    
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl
