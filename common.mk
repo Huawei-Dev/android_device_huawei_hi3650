6dfcc402f4bdbed1365925a56fbaa2397ecba470
Huawei_Dev <surdu_petru@yahoo.com>
2024-06-16 22:23:38 +0200

    hi3650: build camera cfgsvr interface from source

--- common.mk
+++ common.mk
@@ -79,6 +79,9 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64
+
+PRODUCT_PACKAGES += \
+    vendor.huawei.hardware.camera.cfgsvr@1.0.vendor
     
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.2.vendor \
