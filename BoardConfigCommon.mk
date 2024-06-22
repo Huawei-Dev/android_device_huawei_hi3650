a31e1348fa5408826ee82c6392424c53a661d66d
Huawei_Dev <surdu_petru@yahoo.com>
2024-06-22 14:51:03 +0200

    hi3650: update security patch

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -164,7 +164,7 @@ TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):init_hisi
 TARGET_RECOVERY_DEVICE_MODULES := init_hisi
 
 # Vendor Security patch level
-VENDOR_SECURITY_PATCH := 2024-04-05
+VENDOR_SECURITY_PATCH := 2024-06-05
 
 # Verity - Only needed for signing
 BOARD_AVB_ENABLE := false
