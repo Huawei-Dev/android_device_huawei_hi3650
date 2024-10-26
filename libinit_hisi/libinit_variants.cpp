efa908215f2ea85ee3ce7d497f2057e1b70154b8
Huawei_Dev <surdu_petru@yahoo.com>
2024-10-26 21:26:50 +0200

    hi3650: read model info from devicetree

--- libinit_hisi/libinit_variants.cpp
+++ libinit_hisi/libinit_variants.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The LineageOS Project
+ * Copyright (C) 2023-2024 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -7,102 +7,41 @@
 #define LOG_TAG "libinit_variants"
 #include <libinit_utils.h>
 #include <libinit_variants.h>
-
 #include <android-base/logging.h>
-#include <android-base/strings.h>
+#include <fstream>
 
-#include <sstream>
+using namespace std;
 
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <unistd.h>
+constexpr const char* kBoardInfoPath = "/sys/firmware/devicetree/base/hisi,boardname";
 
-struct ProductInfo {
-    std::string model;
-    std::string version;
-    std::string region_type;
-};
+string ReadModelInfo() {
 
-constexpr const char* kOemInfoPath = "/dev/block/by-name/oeminfo";
-std::vector<unsigned char> pattern = {0x4F, 0x45, 0x4D, 0x5F, 0x49, 0x4E, 0x46, 0x4F, 0x06, 0x00,
-                                      0x00, 0x00, 0x4E, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
-                                      0x80, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00};
-
-ProductInfo ParseProductInfo(const std::string& product_info_str) {
-    ProductInfo product_info;
-    std::istringstream iss(product_info_str);
-
-    // Extract the model (i.e. "PRA-LX1").
-    std::getline(iss, product_info.model, ' ');
-
-    // Extract the version (i.e. "9.1.0.311").
-    std::getline(iss, product_info.version, '(');
-
-    // Remove trailing whitespace.
-    if (!product_info.version.empty() && product_info.version.back() == ')') {
-        product_info.version.pop_back();
+    ifstream fd;
+    string buf;
+    
+    fd.open(kBoardInfoPath);
+    if (!fd.is_open()) {
+        LOG(ERROR) << "Unable to open: " << kBoardInfoPath << ", error: " << strerror(errno);
+        return buf;
     }
 
-    // Extract the baseband (i.e. "C185E3R2P1").
-    std::getline(iss, product_info.region_type, ')');
-    return product_info;
-}
+    getline(fd, buf);
+    fd.close();
 
-ProductInfo ReadProductInfo() {
-    ProductInfo product_info = {};
-
-    int fd = open(kOemInfoPath, O_RDONLY);
-    if (fd == -1) {
-        LOG(ERROR) << "Unable to open: " << kOemInfoPath << ", error: " << strerror(errno);
-        return product_info;
-    }
-
-    off_t size = lseek(fd, 0, SEEK_END);
-    lseek(fd, 0, SEEK_SET);  // Go back.
-
-    void* buffer = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (buffer == MAP_FAILED) {
-        LOG(ERROR) << "Unable to map: " << kOemInfoPath << ", error: " << strerror(errno);
-        close(fd);
-        return product_info;
-    }
-
-    auto begin = static_cast<unsigned char*>(buffer);
-    auto end = begin + size;
-    auto it = std::search(begin, end, pattern.begin(), pattern.end());
-
-    if (it != end) {
-        // Skip over 0xFF bytes
-        auto name_start = it + pattern.size();
-        while (*name_start == 0xFF && name_start < end) {
-            ++name_start;
-        }
-        // Parse the product info
-        product_info = ParseProductInfo(std::string(reinterpret_cast<char*>(name_start)));
-    } else {
-        LOG(ERROR) << "Unable to find product name in: " << kOemInfoPath;
-    }
-
-    munmap(buffer, size);
-    close(fd);
-
-    return product_info;
+    return buf;
 }
 
 void load_variants() {
-    ProductInfo product_info = ReadProductInfo();
-    std::string model;
+    string model_info = ReadModelInfo();
 
-    // Load the phone model dynamically from the oeminfo partition.
-    if (!product_info.model.empty()) {
-        LOG(INFO) << "Found product info: " << product_info.model << " " << product_info.version
-                  << " " << product_info.region_type;
-        for (int i = 0; i < 8; i++)
-        	model.push_back(product_info.model[i]);
-        set_ro_build_prop("model", model, true);
+    // Load the phone model dynamically from devicetree.
+    if (!model_info.empty()) {
+        LOG(INFO) << "Found model info: " << model_info;
+        if (model_info.find("BTV_DL09") != string::npos)
+        	set_ro_build_prop("model", "BTV-DL09", true);
+        else if (model_info.find("BTV_W09") != string::npos)
+        	set_ro_build_prop("model", "BTV-W09", true);
     } else {
-        LOG(ERROR) << "Unable to parse product information!";
+        LOG(ERROR) << "Unable to parse model information!";
     }
 }
