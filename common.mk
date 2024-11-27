2dc59af87cdc5f8981ce48167ecc3661b40de8cf
Huawei_Dev <surdu_petru@yahoo.com>
2024-11-27 19:22:34 +0100

    beethoven: revert commit 7ac5d2d

--- common.mk
+++ common.mk
@@ -36,7 +36,6 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     audio.primary.hisi_wrapper \
-    audio.bluetooth.default \
     audio.usb.default \
     audio.r_submix.default
     
@@ -46,7 +45,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.1-impl
     
 PRODUCT_PACKAGES += \
@@ -62,7 +60,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/prebuilts/audio_policy_volumes_drc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes_drc.xml \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
@@ -71,9 +69,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service
-    
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.a2dp@1.0.vendor
     
 PRODUCT_PACKAGES += \
     libbt-vendor
