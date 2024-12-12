1f88af1af289e39ba2332d5c481da4cfbf435a98
Huawei_Dev <surdu_petru@yahoo.com>
2024-12-12 20:17:32 +0100

    hi3650: revert audio to v2.0

--- common.mk
+++ common.mk
@@ -42,10 +42,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio.service.hisi \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.0-impl
     
 PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.0.vendor
